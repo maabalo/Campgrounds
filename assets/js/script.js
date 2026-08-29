edf90b9b126935393dba2320ddeed4f3a46f3a6f
Makoy <92926828+maabalo@users.noreply.github.com>
2026-08-29 23:31:56 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -320,7 +320,7 @@ function renderCards() {
         <h2 class="camp-title">${camp.name || ''}</h2>
         <div class="card-actions">
           <span class="card-location-icon">
-            <svg viewBox="0 0 16 16" width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+            <svg viewBox="0 0 18 18" width="12" height="12" xmlns="http://www.w3.org/2000/svg">
               <path fill="#EB7D00" d="M8 1a5 5 0 00-5 5c0 3.8 5 9 5 9s5-5.2 5-9a5 5 0 00-5-5zm0 7a2 2 0 110-4 2 2 0 010 4z"/>
             </svg>
           </span>
